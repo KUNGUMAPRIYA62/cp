eeb06c3417900b009cc61b8466358265af00c4b2
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-18 22:01:13 +0530

    arithmetic.C
    arithmetic progression

---
+++ arithmetic.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a=3,b=1,c=1,z;
+printf("enter the value of a,b and c");
+scanf("%d%d%d",&a,&b,&c);
+}
+z=a*(b+c);
+{
+printf("arithmetic progression%d",z);
+}
