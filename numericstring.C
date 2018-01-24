0128cc43418a4f1d5ebe5f2b40c25dec55604731
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-01-24 16:09:09 +0530

    numericstring.C
    numeric

---
+++ numericstring.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char a[10];
+	int i,l,count=0;
+	scanf("%s",a);
+	l=strlen(a);
+	for(i=0;i<l;i++)
+	{
+	if(a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9'||a[i]=='0')
+{
+	count++;
+}
+}
+	if(count==0)
+	printf("no");
+	else
+	printf("yes");
+}
